be0151854d18de8b54ec9abed89b272bbff582af
Og B. Maciel <ogmaciel@gnome.org>
2013-10-16 17:41:53 -0400

    Change hostname to localhost:3000 in some config files.

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,13 +19,16 @@ yum install -y vim-enhanced
 yum update -y selinux-policy-targeted
 
 # Configure the hostname
-IP=`ifconfig | grep broadcast | awk -F ' ' '{print $2}'`
-echo "$IP vagrant-fedora-19 vagrant-fedora-19.vagrantup.com" >> /etc/hosts
+echo "127.0.0.1 localhost vagrant-fedora-19 vagrant-fedora-19.vagrantup.com" >> /etc/hosts
 echo "HOSTNAME=vagrant-fedora-19.vagrantup.com" > /etc/sysconfig/network
 hostname vagrant-fedora-19.vagrantup.com
 
 # Configure katello
 katello-configure --user-pass admin
+sed -i -e 's/vagrant-fedora-19.vagrantup.com/localhost:3000/' /etc/katello/katello.yml
+sed -i -e 's/vagrant-fedora-19.vagrantup.com/localhost:3000/' /etc/signo/sso.yml
+sed -i -e 's/vagrant-fedora-19.vagrantup.com/localhost/' /etc/pulp/server.conf
+katello-service restart
 
 # Keep SELinux off after a reboot
 sed -i -e 's/^SELINUX=*/SELINUX=permissive/' /etc/selinux/config
